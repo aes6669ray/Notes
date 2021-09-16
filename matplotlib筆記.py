3c6c52799d3138294174f31dbaa7d7d4054cac08
aes6669ray <aes6669ray@gmail.com>
2021-09-17 01:39:56 +0800

    init

--- matplotlib筆記.py
+++ matplotlib筆記.py
@@ -17,3 +17,6 @@ ax.spines['top'].set_visible(False)   #看不到上面的線
 ax.spines["bottom"].set_position(("data",1)) #設定底下的線(x軸)在y=1的位置
 
 plt.legend(handle=[],labels=[],loc="best")  #handle代表可以自己設定要的參數(丟進去的資料),best代表放在最好的位置,也可以統一放在upper right;botton left
+plt.subplots_adjust(wspace=0,hspace=0)  ##可以調整subplot的各個間隔
+
+plt.annotate(lable, xy=(date))      ##給圖片新增文字標示,xy參數代表要放的座標位置(符號標誌),xytext則是label位置, //annotate比較複雜,可以配合影片一起操作
